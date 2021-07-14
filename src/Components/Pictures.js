40175e77830e33164b3b91801dea4f8b2451e324
github username <github email address>
2021-07-14 08:56:51 -0400

    tweaks

--- src/Components/Pictures.js
+++ src/Components/Pictures.js
@@ -4,7 +4,7 @@ import { Button } from "semantic-ui-react";
 const Pictures = ({ feed, user, handleDelete }) => {
   const filteredPosts = feed.filter((post) => post.author === user);
   return (
-    <div>
+    <div className="myPicList">
       <h1>My Art</h1>
       {filteredPosts.map((pic) => {
         return (
