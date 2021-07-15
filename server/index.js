0378c9f9cf462d1669e4ae014eebe653e19725a3
Henry <hhpendleton@gmail.com>
2021-07-15 15:30:24 -0400

    server added

--- server/index.js
+++ server/index.js
@@ -1,23 +1,57 @@
-const express = require("express");
+import express from "express";
+import bodyParser from "body-parser";
+import mysql from "mysql";
+import cors from "cors";
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const path = require("path");
-
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
-app.use(express.static("api"));
-// Handle GET requests to /api route
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-app.get("/api", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "pictogramDB",
 });
 
-// All other GET requests not handled before will return our React app
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+app.use(cors());
+app.use(express.json());
+require("dotenv").config({ path: __dirname + "/.env" });
+
+app.get("/api/get", (req, res) => {
+  const sqlSelect = "SELECT * FROM user_posts";
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
+  });
+});
+
+app.post("/api/insert", (req, res) => {
+  const title = req.body.title;
+  const author = req.body.author;
+  const image = req.body.image;
+  const datePosted = req.body.datePosted;
+  const sqlInsert =
+    "INSERT INTO user_posts (title, author, image, datePosted) VALUE (?,?,?,?)";
+  db.query(sqlInsert, [title, author, image, datePosted], (err, result) => {
+    console.log(err, result);
+  });
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  db.query(
+    "DELETE FROM user_posts WHERE iduser_posts = ?",
+    id,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
